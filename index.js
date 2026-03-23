d17ed2482a602ae73a8fb489f0f8313819b0905c
Samuel <gachengasam@gmail.com>
2026-03-23 11:09:40 +0300

    update changes

--- index.js
+++ index.js
@@ -12,3 +12,43 @@ module.exports = {
   ...(typeof removeEventProperty !== 'undefined' && { removeEventProperty }),
   ...(typeof addCheckedInProperty !== 'undefined' && { addCheckedInProperty })
 };
+
+// atendee object
+const attendee = {
+  attendeeId: `T001`,
+  name: `Alice Smith`,
+  event: `JavaScript`,
+  ticketType: `VIP`,
+  ticketPrice: 150
+};
+
+// logAttendeeName function
+function logAttendeeName() {
+  console.log(attendee.name);
+};
+
+// logTicketPrice function
+function logTicketPrice() {
+  console.log(attendee.ticketPrice);
+};
+
+// updateTicketType function
+function updateTicketType(newType) {
+  attendee.ticketType = newType;
+};
+
+// updateTicketPrice function
+function updateTicketPrice(newPrice) {
+  attendee.ticketPrice = newPrice;
+};
+
+// removeEventProperty function
+function removeEventProperty() {
+  delete attendee.event;
+};
+
+// addCheckedInProperty function
+function addCheckedInProperty() {
+  attendee.checkedIn = true;
+};
+
